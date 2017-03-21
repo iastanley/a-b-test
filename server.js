76251b2472cf1e6b10edd380c599f3516f5ca44e
iastanley <iasvks.craigslist@gmail.com>
2017-03-20 18:56:17 -0700

    added random setting of cookie functionality

--- server.js
+++ server.js
@@ -7,9 +7,24 @@ app.use(cookieParser());
 //required to use js and css files in public directory
 app.use(express.static('public'));
 
+const COOKIE_NAME = 'a-b-test';
+
 app.get('/', (req, res) => {
+  //set cookie if not set already
+  if (!req.cookies[COOKIE_NAME]) {
+    res.cookie(COOKIE_NAME, randomAB());
+  }
   //sendFile needed for serving html files from views directory
   res.sendFile(`${__dirname}/views/index.html`);
 });
+
+//random 'a' 'b' generator
+const randomAB = () => {
+  if (Math.random() > 0.5) {
+    return 'a';
+  } else {
+    return 'b';
+  }
+}
 
 app.listen(3000, () => console.log('Server has started at 3000'));
