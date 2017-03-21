682cdaceae566e3eeb96c1e2e5b7c7c032d46fd5
iastanley <iasvks.craigslist@gmail.com>
2017-03-20 18:10:01 -0700

    added ability to use files from public and views in server.js

--- server.js
+++ server.js
@@ -3,8 +3,13 @@ const express = require('express');
 const cookieParser = require('cookie-parser');
 const app = express();
 
-
 app.use(cookieParser());
+//required to use js and css files in public directory
+app.use(express.static('public'));
 
+app.get('/', (req, res) => {
+  //sendFile needed for serving html files from views directory
+  res.sendFile(`${__dirname}/views/index.html`);
+});
 
 app.listen(3000, () => console.log('Server has started at 3000'));
